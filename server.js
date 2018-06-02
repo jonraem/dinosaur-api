14c4d8167a2090cf8a515bc3fd068280a0500ef4
jonraem <joni.ramo.91@gmail.com>
2018-06-02 13:14:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world! And dinosaurs...'));
+app.get('/', (req, res) => res.send('Hello world! 🦕'));
 
 const port = 3000;
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`Dinosaurs sighted on port ${port}!`))
