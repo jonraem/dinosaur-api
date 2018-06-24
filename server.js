4092917571feca04126a7e8179a275089d46c4e0
jonraem <joni.ramo.91@gmail.com>
2018-06-24 13:39:23 +0300

    Connect MongoDB to server

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 
+const db = require('./config/db');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello world!  🦕'));
-
 app.use(bodyParser.urlencoded({ extended: true }));
-require('./app/routes')(app, {});
-app.listen(port, () => console.log(`Dinosaurs sighted on port ${port}!`))
+
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err);
+  const dbRef = database.db('dinosaur-db');
+
+  require("./app/routes")(app, dbRef);
+  app.listen(port, () => {
+    console.log(`Dinosaurs sighted on port ${port}!`);
+  });
+});
