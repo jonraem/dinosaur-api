d0bcc357bef08a1f6bed924bcaa0f27dc16e2fd8
jonraem <joni.ramo.91@gmail.com>
2018-06-24 13:39:43 +0300

    Add actual POST logic

--- app/routes/dinosaur.js
+++ app/routes/dinosaur.js
@@ -1,6 +1,12 @@
 module.exports = function(app, db) {
   app.post('/dinosaurs', (req, res) => {
-    // You create your dinosaur here.
-    res.send('Rawr! 🦖')
+    const dinosaur = { name: req.body.name, type: req.body.type };
+    db.collection('dinosaurs').insert(dinosaur, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred.' });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
   });
 };
