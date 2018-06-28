71a6156e31c94cc88d1820f6afc57c48de92191b
jonraem <joni.ramo.91@gmail.com>
2018-06-28 22:26:55 +0300

    Add GET route

--- app/routes/dinosaur.js
+++ app/routes/dinosaur.js
@@ -1,4 +1,20 @@
+var ObjectID = require("mongodb").ObjectID;
+
 module.exports = function(app, db) {
+  // GET
+  app.get('/dinosaurs/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('dinosaurs').findOne(details, (err, item) => {
+      if (err) {
+          res.send({ 'error': 'An error has occurred' });
+        } else {
+          res.send(item);
+      }
+      });
+    });
+  
+    // POST
   app.post('/dinosaurs', (req, res) => {
     const dinosaur = { name: req.body.name, type: req.body.type };
     db.collection('dinosaurs').insert(dinosaur, (err, result) => {
