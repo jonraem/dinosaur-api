5cd13aa1899b5ce01be2a7564735359c0c2a2c08
Joni Rämö <joni.ramo.91@gmail.com>
2019-02-09 21:55:05 +0200

    Update Mongo connect

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 
-const db = require('./config/db');
+const db = require('./config/db.json');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(db.url, (err, database) => {
+MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
   if (err) return console.log(err);
   const dbRef = database.db('dinosaur-db');
 
